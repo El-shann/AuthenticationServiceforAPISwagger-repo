cf16a92dcc67fa7fb7b51aa0f381a2554b7828e0
Elshan M <118193694+El-shann@users.noreply.github.com>
2022-12-24 14:40:05 +0300

    Add files via upload

---
+++ UsersController.cs
@@ -0,0 +1,40 @@
+﻿using BasicAuthService.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BasicAuthService.Controllers
+{
+    [Authorize]
+    [ApiVersion("1.0")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        List<User> _users = new List<User>();
+        public UsersController()
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                _users.Add(new User()
+                {
+                    userID = i,
+                    name = "stu" + i,
+                    Roll = "100" + i
+                });
+
+            }
+        }
+        // GET: api/<UsersController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _users;
+        }
+    }
+}
+
+    
+
+      
